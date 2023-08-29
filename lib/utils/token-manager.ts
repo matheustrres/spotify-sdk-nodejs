e811835120a9fd3d237b82d597fc4995ae08a6da
Matheus <torresmiranda.rj@gmail.com>
2023-08-28 23:21:20 -0300

    feat: create manager for handling spotify client token properly

---
+++ lib/utils/token-manager.ts
@@ -0,0 +1,57 @@
+import { type Response, type SpotifyApiClientCredentials } from '../typings';
+import { makePOST } from './request';
+
+export interface ITokenManager {
+	getAuthToken(): Promise<string>;
+}
+
+export class TokenManager implements ITokenManager {
+	private readonly basicAuthCredentials: string; // base64 encoded credential
+
+	private authToken: string; // Bearer <token>
+	private tokenExpirationDate: number;
+
+	constructor(clientId: string, clientSecret: string) {
+		this.basicAuthCredentials = Buffer.from(
+			`${clientId}:${clientSecret}`,
+		).toString('base64');
+
+		this.tokenExpirationDate = 0;
+		this.authToken = '';
+	}
+
+	public async getAuthToken(): Promise<string> {
+		if (!this.authToken || this.isAuthTokenExpired()) {
+			await this.fetchClientToken();
+		}
+
+		return this.authToken;
+	}
+
+	private isAuthTokenExpired(): boolean {
+		return (
+			this.tokenExpirationDate === 0 || Date.now() > this.tokenExpirationDate
+		);
+	}
+
+	private async fetchClientToken(): Promise<void> {
+		const response: Response<SpotifyApiClientCredentials> = await makePOST(
+			'https://accounts.spotify.com/api/token?grant_type=client_credentials',
+			{
+				headers: {
+					Authorization: `Basic ${this.basicAuthCredentials}`,
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			},
+		);
+
+		if (response.error) {
+			throw new Error(
+				`Error while fetching client token: "${response.error.message}"`,
+			);
+		}
+
+		this.authToken = `${response.token_type} ${response.access_token}`;
+		this.tokenExpirationDate = Date.now() + response.expires_in * 1000 - 5000;
+	}
+}
