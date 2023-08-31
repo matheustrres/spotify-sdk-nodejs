952c830edbd560066cce4aca1c82acb5479dfc7b
Matheus <torresmiranda.rj@gmail.com>
2023-08-31 11:03:15 -0300

    test(sdk): ensure sdk returns info for a single album

---
+++ tests/spotify-sdk.spec.ts
@@ -0,0 +1,37 @@
+import { SpotifySDK } from '../lib/spotify-sdk';
+// ------------------
+// fixtures
+// ------------------
+import spotifyApiGetAlbumResponse from './fixtures/albums/spotify_api_get_album_response.json';
+
+describe('SpotifySDK', (): void => {
+	let sdk: SpotifySDK;
+
+	beforeAll((): void => {
+		sdk = new SpotifySDK({
+			clientId: process.env.SPOTIFY_CLIENT_ID as string,
+			clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+		});
+	});
+
+	it('should be defined', (): void => {
+		expect(sdk).toBeDefined();
+		expect(sdk.albums).toBeDefined();
+		expect(sdk.artists).toBeDefined();
+		expect(sdk.tracks).toBeDefined();
+	});
+
+	describe('.albums', (): void => {
+		describe('.getAlbum', (): void => {
+			it('should return spotify catalog information for a album', async (): Promise<void> => {
+				const { data, error } = await sdk.albums.getAlbum(
+					'4aawyAB9vmqN3uQ7FjRGTy',
+				);
+
+				expect(error).toBe(undefined);
+				expect(data).toBeDefined();
+				expect(data).toMatchObject(spotifyApiGetAlbumResponse);
+			});
+		});
+	});
+});
