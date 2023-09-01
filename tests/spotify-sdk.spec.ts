7a7c7e7a635ecc16811b2cbce26bc0172acfb3a4
Matheus <torresmiranda.rj@gmail.com>
2023-09-01 19:26:34 -0300

    feat(tests): mock resolved value for tests

--- tests/spotify-sdk.spec.ts
+++ tests/spotify-sdk.spec.ts
@@ -1,4 +1,10 @@
+import { type MockProxy, mock } from 'jest-mock-extended';
+
+import { type IHttpClient } from '../lib/http-client';
 import { SpotifySDK } from '../lib/spotify-sdk';
+// ------------------
+// fixtures
+// ------------------
 // ------------------
 // fixtures/albums
 // ------------------
@@ -13,6 +19,7 @@ import spotifyApiGetArtistAlbumsResponse from './fixtures/artists/spotify_api_ge
 import spotifyApiGetArtistResponse from './fixtures/artists/spotify_api_get_artist_response.json';
 import spotifyApiGetArtistTopTracksResponse from './fixtures/artists/spotify_api_get_artist_top_tracks_response.json';
 import spotifyApiGetSeveralArtistsResponse from './fixtures/artists/spotify_api_get_several_artists_response.json';
+import spotifyApiClientCredentialsResponse from './fixtures/spotify_api_client_credentials_response.json';
 // ------------------
 // fixtures/tracks
 // ------------------
@@ -23,13 +30,37 @@ import spotifyApiGetTrackResponse from './fixtures/tracks/spotify_api_get_track_
 import spotifyApiGetTracksAudioFeaturesResponse from './fixtures/tracks/spotify_api_get_tracks_audio_features_response.json';
 
 describe('SpotifySDK', (): void => {
+	let httpClient: MockProxy<IHttpClient>;
 	let sdk: SpotifySDK;
 
 	beforeAll((): void => {
-		sdk = new SpotifySDK({
-			clientId: process.env.SPOTIFY_CLIENT_ID as string,
-			clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
-		});
+		httpClient = mock();
+
+		httpClient.POST.mockResolvedValue(spotifyApiClientCredentialsResponse);
+
+		httpClient.GET.mockResolvedValueOnce(spotifyApiGetAlbumResponse)
+			.mockResolvedValueOnce(spotifyApiGetAlbumTracksResponse)
+			.mockResolvedValueOnce(spotifyApiGetNewsReleasesResponse)
+			.mockResolvedValueOnce(spotifyApiGetSeveralAlbumsResponse)
+			.mockResolvedValueOnce(spotifyApiGetArtistResponse)
+			.mockResolvedValueOnce(spotifyApiGetArtistAlbumsResponse)
+			.mockResolvedValueOnce(spotifyApiGetArtistTopTracksResponse)
+			.mockResolvedValueOnce(spotifyApiGetSeveralArtistsResponse)
+			.mockResolvedValueOnce(spotifyApiGetSeveralTracksResponse)
+			.mockResolvedValueOnce(spotifyApiGetTrackResponse)
+			.mockResolvedValueOnce(spotifyApiGetTrackAudioFeatures)
+			.mockResolvedValueOnce(spotifyApiGetTracksAudioFeaturesResponse)
+			.mockResolvedValueOnce(spotifyApiGetTrackAudioAnalysisResponse);
+	});
+
+	beforeEach((): void => {
+		sdk = new SpotifySDK(
+			{
+				clientId: process.env.SPOTIFY_CLIENT_ID as string,
+				clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+			},
+			httpClient,
+		);
 	});
 
 	it('should be defined', (): void => {
@@ -42,102 +73,98 @@ describe('SpotifySDK', (): void => {
 	describe('.albums', (): void => {
 		describe('.getAlbum', (): void => {
 			it('should return spotify catalog information for a single album', async (): Promise<void> => {
-				const { data, error } = await sdk.albums.getAlbum(
-					'4aawyAB9vmqN3uQ7FjRGTy',
-				);
+				const result = await sdk.albums.getAlbum('4aawyAB9vmqN3uQ7FjRGTy');
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetAlbumResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetAlbumResponse);
 			});
 		});
 
 		describe('.getAlbumTracks', (): void => {
 			it("should return spotify catalog information about an album's tracks", async (): Promise<void> => {
-				const { data, error } = await sdk.albums.getAlbumTracks(
+				const result = await sdk.albums.getAlbumTracks(
 					'4aawyAB9vmqN3uQ7FjRGTy',
 				);
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetAlbumTracksResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetAlbumTracksResponse);
 			});
 		});
 
 		describe('.getNewReleases', (): void => {
-			it.skip('should return a list of new album releases featured in Spotify', async (): Promise<void> => {
-				const { data, error } = await sdk.albums.getNewReleases('SE', {
+			it('should return a list of new album releases featured in Spotify', async (): Promise<void> => {
+				const result = await sdk.albums.getNewReleases('SE', {
 					limit: 5,
 					offset: 2,
 				});
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetNewsReleasesResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetNewsReleasesResponse);
 			});
 		});
 
 		describe('.getSeveralAlbums', (): void => {
 			it('should return spotify catalog information for multiple albums', async (): Promise<void> => {
-				const { data, error } = await sdk.albums.getSeveralAlbums([
+				const result = await sdk.albums.getSeveralAlbums([
 					'382ObEPsp2rxGrnsizN5TX',
 					'1A2GTWGtFfWp7KSQTwWOyo',
 				]);
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetSeveralAlbumsResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetSeveralAlbumsResponse);
 			});
 		});
 	});
 
 	describe('.artists', (): void => {
 		describe('.getArtist', (): void => {
-			it.skip('should return spotify catalog information for a single artist', async (): Promise<void> => {
-				const { data, error } = await sdk.artists.getArtist(
-					'0TnOYISbd1XYRBk9myaseg',
-				);
+			it('should return spotify catalog information for a single artist', async (): Promise<void> => {
+				const result = await sdk.artists.getArtist('0TnOYISbd1XYRBk9myaseg');
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetArtistResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetArtistResponse);
 			});
 		});
 
 		describe('.getArtistAlbums', (): void => {
-			it.skip("should return spotify catalog information about an artist's albums", async (): Promise<void> => {
-				const { data, error } = await sdk.artists.getArtistAlbums(
+			it("should return spotify catalog information about an artist's albums", async (): Promise<void> => {
+				const result = await sdk.artists.getArtistAlbums(
 					'0TnOYISbd1XYRBk9myaseg',
 				);
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetArtistAlbumsResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetArtistAlbumsResponse);
 			});
 		});
 
 		describe('.getArtistTopTracks', (): void => {
-			it.skip('should return spotify catalog information about an artist top tracks', async (): Promise<void> => {
-				const { data, error } = await sdk.artists.getArtistTopTracks(
+			it('should return spotify catalog information about an artist top tracks', async (): Promise<void> => {
+				const result = await sdk.artists.getArtistTopTracks(
 					'0TnOYISbd1XYRBk9myaseg',
 				);
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetArtistTopTracksResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetArtistTopTracksResponse);
 			});
 		});
 
 		describe('.getSeveralArtists', (): void => {
-			it.skip('should return spotify catalog information for several artists', async (): Promise<void> => {
-				const { data, error } = await sdk.artists.getSeveralArtists([
+			it('should return spotify catalog information for several artists', async (): Promise<void> => {
+				const result = await sdk.artists.getSeveralArtists([
 					'0TnOYISbd1XYRBk9myaseg',
 					'57dN52uHvrHOxijzpIgu3E',
 				]);
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetSeveralArtistsResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetSeveralArtistsResponse);
 			});
 		});
 	});
@@ -145,64 +172,66 @@ describe('SpotifySDK', (): void => {
 	describe('.tracks', (): void => {
 		describe('.getSeveralTracks', (): void => {
 			it('should return spotify catalog information for several tracks', async (): Promise<void> => {
-				const { data, error } = await sdk.tracks.getSeveralTracks([
+				const result = await sdk.tracks.getSeveralTracks([
 					'7ouMYWpwJ422jRcDASZB7P',
 					'4VqPOruhp5EdPBeR92t6lQ',
 					'2takcwOaAZWiXQijPHIx7B',
 				]);
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetSeveralTracksResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetSeveralTracksResponse);
 			});
 		});
 
 		describe('.getTrack', (): void => {
 			it('should return spotify catalog information for a single track', async (): Promise<void> => {
-				const { data, error } = await sdk.tracks.getTrack(
-					'7ouMYWpwJ422jRcDASZB7P',
-				);
+				const result = await sdk.tracks.getTrack('7ouMYWpwJ422jRcDASZB7P');
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetTrackResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetTrackResponse);
 			});
 		});
 
 		describe('.getTrackAudioFeatures', (): void => {
 			it('should return audio feature information for a single track', async (): Promise<void> => {
-				const { data, error } = await sdk.tracks.getTrackAudioFeatures(
+				const result = await sdk.tracks.getTrackAudioFeatures(
 					'7ouMYWpwJ422jRcDASZB7P',
 				);
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetTrackAudioFeatures);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(spotifyApiGetTrackAudioFeatures);
 			});
 		});
 
 		describe('.getTracksAudioFeatures', (): void => {
 			it('should return audio features information for multiple tracks', async (): Promise<void> => {
-				const { data, error } = await sdk.tracks.getTracksAudioFeatures([
+				const result = await sdk.tracks.getTracksAudioFeatures([
 					'7ouMYWpwJ422jRcDASZB7P',
 					'4VqPOruhp5EdPBeR92t6lQ',
 				]);
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetTracksAudioFeaturesResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(
+					spotifyApiGetTracksAudioFeaturesResponse,
+				);
 			});
 		});
 
 		describe('.getTrackAudioAnalysis', (): void => {
 			it('should return a low-level audio analysis for a track', async (): Promise<void> => {
-				const { data, error } = await sdk.tracks.getTrackAudioAnalysis(
+				const result = await sdk.tracks.getTrackAudioAnalysis(
 					'7ouMYWpwJ422jRcDASZB7P',
 				);
 
-				expect(error).toBe(undefined);
-				expect(data).toBeDefined();
-				expect(data).toMatchObject(spotifyApiGetTrackAudioAnalysisResponse);
+				expect(result.error).toBe(undefined);
+				expect(result.timestamp).toBeDefined();
+				expect(result.data).toMatchObject(
+					spotifyApiGetTrackAudioAnalysisResponse,
+				);
 			});
 		});
 	});
