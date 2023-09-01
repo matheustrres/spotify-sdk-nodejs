399588725647d99ef3f0c32c491c138974a28fc7
Matheus <torresmiranda.rj@gmail.com>
2023-09-01 12:29:31 -0300

    test(request): ensure GET is called with correct params

---
+++ tests/request.spec.ts
@@ -0,0 +1,52 @@
+import { makeGET } from '../lib/request';
+import { SpotifyTokenManager } from '../lib/utils/token-manager';
+import spotifyApiGetArtistResponse from './fixtures/artists/spotify_api_get_artist_response.json';
+
+const mockFetchResponse = <T>(response: T): jest.Mock =>
+	jest.fn().mockImplementation(async () => {
+		return {
+			json: async (): Promise<T> => response,
+		};
+	});
+
+describe('Request', (): void => {
+	const baseURL: string = 'https://api.spotify.com/v1';
+
+	let tokenManager: SpotifyTokenManager;
+	let authToken: string;
+
+	afterAll((): void => {
+		jest.clearAllMocks();
+	});
+
+	beforeAll(async (): Promise<void> => {
+		tokenManager = new SpotifyTokenManager(
+			process.env.SPOTIFY_CLIENT_ID as string,
+			process.env.SPOTIFY_CLIENT_SECRET as string,
+		);
+
+		authToken = await tokenManager.getAuthToken();
+	});
+
+	describe('X GET', (): void => {
+		it('should call GET with correct parameters', async (): Promise<void> => {
+			global.fetch = mockFetchResponse(spotifyApiGetArtistResponse);
+
+			await makeGET(`${baseURL}/artists/0TnOYISbd1XYRBk9myaseg`, {
+				headers: {
+					Authorization: authToken,
+				},
+			});
+
+			expect(global.fetch).toHaveBeenCalledWith(
+				new URL(`${baseURL}/artists/0TnOYISbd1XYRBk9myaseg`),
+				{
+					method: 'GET',
+					headers: {
+						Authorization: authToken,
+					},
+				},
+			);
+		});
+	});
+});
